01d35ae9662556b18c26c637ee94c2466d61d5c7
Dejun Lin <dejun.lin@gmail.com>
2015-02-16 16:53:51 -0500

    my Electric MultiPole template codes

---
+++ emp.hpp
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  emp.h
+ *
+ *    Description:  Electric MultiPole tensor class
+ *
+ *        Version:  1.0
+ *        Created:  02/16/2015 10:21:50 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dejun Lin (DL), dejun.lin@gmail.com
+ *   Organization:  Department of Biochemistry and Biophysics, Medical Center, University of Rochester
+ *
+ * =====================================================================================
+ */
+#include <array>
+#include "typedefs.hpp"
+using namespace std;
+
+template <size_t R>
+class EMP {
+  constexpr size_t   
+};
