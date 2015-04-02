33f26a4eedcbba35babced73fcc2c7410bdca872
Dejun Lin <dejun.lin@gmail.com>
2015-04-02 16:58:15 -0400

    add CMake and build script

---
+++ build_release.sh
@@ -0,0 +1,9 @@
+#!/bin/tcsh
+setenv PREFIX "/usr/local"
+setenv LIBDIR "$PREFIX/lib"
+setenv LIB64DIR "$PREFIX/lib64"
+setenv CC "gcc"
+setenv CXX "g++"
+setenv CPPFLAGS "-I$PREFIX/include"
+setenv LDFLAGS "-L$LIBDIR -L$LIB64DIR"
+cmake .. -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_BUILD_TYPE=Release 
